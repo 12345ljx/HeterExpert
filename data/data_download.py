85272f8667ace0f8801d8ed4bafbd4c4bd72fced
12345ljx <1064423904@qq.com>
2025-05-21 21:47:05 +0800

    update

--- data/data_download.py
+++ data/data_download.py
@@ -4,7 +4,7 @@ from datasets import load_dataset, DownloadConfig
 
 config = DownloadConfig(resume_download=True) 
 dataset = load_dataset('Salesforce/wikitext', 'wikitext-2-raw-v1', download_config=config)
-dataset.save_to_disk('./datasets/wikitext2-raw')
+dataset.save_to_disk('./data/wikitext2-raw')
 
 print(dataset)
 
